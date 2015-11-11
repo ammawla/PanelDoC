8518ca0f8c7edc05bf83c1ef6b0aedc28713cacb
Alex <ammawla@users.noreply.github.com>
2015-11-11 13:57:59 -0800

    Update DoC_parameters.R

--- Scripts/DoC_parameters.R
+++ Scripts/DoC_parameters.R
@@ -1,6 +1,8 @@
-### PanelDoC v 1.1 Parameters
-### Authors: Alex S. Nord, Alex M. Mawla Copyright 2011-2015
-### Runs invariant set normalization and calls CNVs based on parameters
+### PanelDoC v 1.2 Parameters ###
+### Authors: Alex S. Nord, Alex M. Mawla
+### Copyright 2011-2015
+### Runs invariant set normalization, gc-content normalization, and calls CNVs based on parameters
+
 #
 # Notes:
 #
@@ -50,39 +52,39 @@ experiment.name = "CNV_Tutorial",
 
 ### Set file locations (provide full path) ###
 # directory where coverage/ratio data is stored 
-experiment.directory = "{Path to Parent Dir}/CNV_Tutorial",
+experiment.directory = "{PATH}/PanelDoC/",
 
 # directory where platform data is stored
-platform.directory = "{Path to Parent Dir}/CNV_Tutorial/Platform",
+platform.directory = "{PATH}/PanelDoC/Platform",
 
 # directory where genome information is stored
-genome.directory = "{Path to UCSC Dir}/UCSC/hg19/sequence", 
+genome.directory = "{PATH}/PanelDoC/UCSC/hg19/sequence", 
 
 # directory where gene annotation information is stored
-annotation.directory = "{Path to UCSC Dir}/UCSC/hg19/annotation",
+annotation.directory = "{PATH}/PanelDoC/UCSC/hg19/annotation",
 
 ### Set input data filenames (must match expected format)
 # file with sample information for processing batch in csv format
-sample.filename = "{Path to Parent Dir}/CNV_Tutorial/Input/SampleInformation.csv",
+sample.filename = "{PATH}/PanelDoC/Input/SampleInformation.csv",
 
 # file with information for bait probes in bed format
-bait.filename = "{Path to Parent Dir}/CNV_Tutorial/Input/baitcoverage.bed",
+bait.filename = "{PATH}/PanelDoC/Input/baitcoverage.bed",
 
 # file with data on how genomic coverage data is stored (typically chromosomes)
-partition.filename = "{Path to Parent Dir}/CNV_Tutorial/Input/Platform_partitionInformation.csv",
+partition.filename = "{PATH}/PanelDoC/Input/Platform_partitionInformation.csv",
 
 # file with all known CNVs for normalization purposes (including large known CNVs should improve normalization)
-knowncnvs.filename = "{Path to Parent Dir}/CNV_Tutorial/Input/knowncnvs.csv",
+knowncnvs.filename = "{PATH}/PanelDoC/Input/knowncnvs.csv",
  
 # refesq file from UCSC used for plotting (should be in annotation.directory)
-gene.filename = "{Path to UCSC Dir}/UCSC/hg19/annotation/refFlat.txt",
+gene.filename = "{PATH}/PanelDoC/UCSC/hg19/annotation/refFlat.txt",
  
 # source file for functions 
-source.filename = "{Path to Parent Dir}/CNV_Tutorial/Scripts/DoC_functions.R",
+source.filename = "{PATH}/PanelDoC/Scripts/DoC_functions.R",
 
 ### Set criteria for analysis ###
 prep.partition = TRUE, # if TRUE generate genomic data for bed platform, else already present
-generate.median = TRUE, # if TRUE, then generate raw median coverage data	
+generate.median = TRUE, # if TRUE, then generate raw median coverage data
 gene.graphics = TRUE, # if partitions represent genes, then TRUE, else FALSE.  If true, gene is plotted in graphical output
 output.bedgraph = TRUE, # if TRUE, bedgraphs are written for raw coverage and normalized ratio data  
 output.normalized = TRUE, # if TRUE, output data is written for raw and normalized coverage.  
@@ -107,6 +109,7 @@ gain.extend.signal = 1.2, # expected signal for gain seed extension
 loss.extend.signal = .8, # expected signal for loss seed extension
 gain.call.signal = 1.3, # signal criteria for gain call
 loss.call.signal = .7,  # signal criteria for loss call
+minimum.zscore = 1, # minimum z score for gc content normalization
 minimum.base.window = 200, # minimum window base count for call
 minimum.base.pass = 180, # minimum base count that pass criteria for call
 window.size = 50, # window size for scanning
